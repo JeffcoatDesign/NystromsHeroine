2eb7f67e5720282158d453815ca78f2fa31b142a
JeffcoatDesign <98057241+JeffcoatDesign@users.noreply.github.com>
2023-09-08 15:37:32 -0700

    Tweaks

--- Nystrom's Heroine/Assets/Scripts/BackwardsState.cs
+++ Nystrom's Heroine/Assets/Scripts/BackwardsState.cs
@@ -5,9 +5,12 @@ public class BackwardsState : HeroineState
     private bool _isMoving = true;
     private float _reverseSpeed = 300f;
     private Rigidbody _rb;
+    private float _moveStartTime;
+    private float _moveWaitTime = 0.2f;
 
     public override void Enter(Heroine heroine)
     {
+        _moveStartTime = Time.time;
         _rb = heroine.GetComponent<Rigidbody>();
         if (_rb != null)
             _rb.AddForce(_reverseSpeed * -Vector3.forward);
@@ -15,7 +18,7 @@ public class BackwardsState : HeroineState
 
     public override HeroineState HandleInput(Heroine heroine)
     {
-        if (!_isMoving)
+        if (!_isMoving && (Time.time - _moveStartTime > _moveWaitTime))
             return new AlteredState();
         return null;
     }
--- Nystrom's Heroine/Assets/Scripts/ForwardState.cs
+++ Nystrom's Heroine/Assets/Scripts/ForwardState.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class ForwardState : HeroineState
 {
     private bool _isMoving = true;
-    private float _forwardSpeed = 500f;
+    private float _forwardSpeed = 400f;
     private Rigidbody _rb;
+    private float _moveStartTime;
+    private float _moveWaitTime = 0.2f;
 
     public override void Enter(Heroine heroine)
     {
+        _moveStartTime = Time.time;
         _rb = heroine.GetComponent<Rigidbody>();
         if (_rb != null)
             _rb.AddForce(_forwardSpeed * Vector3.forward);
@@ -15,7 +18,7 @@ public class ForwardState : HeroineState
 
     public override HeroineState HandleInput(Heroine heroine)
     {
-        if (!_isMoving)
+        if (!_isMoving && (Time.time - _moveStartTime > _moveWaitTime))
             return new AlteredState();
         return null;
     }
